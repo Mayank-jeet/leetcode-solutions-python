dd35f7392123527a2c8e8a42df36aff1bd55237f
Mayank Jeet <mayankjeet7656@gmail.com>
2026-09-19 22:53:18 +0530

    Add LeetCode 1401 solution

---
+++ Math/1015.Smallest_Integer_Divisible_by_K.py
@@ -0,0 +1,19 @@
+""" 
+* LeetCode: 1015 - Smallest Integer Divisible by K
+ * Link: https://leetcode.com/problems/smallest-integer-divisible-by-k/
+ * Difficulty: Medium
+ * Time: O(k)
+ * Space: O(1)
+"""
+from collections import deque, defaultdict, Counter
+from typing import List, Optional
+import heapq
+class Solution:
+    def smallestRepunitDivByK(self, k: int) -> int:
+        if k==1: return 1
+        if k%2==0 or k%5==0: return -1
+        rem=0
+        for i in range(1,k+1):
+            rem=(rem*10+1)%k
+            if rem==0: return i
+        return -1
